0d8035b1a53e46c43358f635fd3347d72535aa19
atus <tudaps39496@gmail.com>
2025-11-20 22:02:13 +0700

    Lưu thay đổi tạm để chuyển nhánh

--- shoeStore/src/main/java/com/fpl/edu/shoeStore/product/productEntity.java
+++ shoeStore/src/main/java/com/fpl/edu/shoeStore/product/productEntity.java
@@ -1,4 +1,14 @@
 package com.fpl.edu.shoeStore.product;
 
-public class entity {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class productEntity {
+    private int productId;
 }
